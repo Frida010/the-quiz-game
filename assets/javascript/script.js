68868a3ffd5a6cf00cff19877254e79e6023c52d
Frida010 <>
2023-07-31 18:53:28 +0000

    add questions and answers to script file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,33 @@
 // Questions and answers for game area
 
+const questions = [
+
+    {
+        question: "What does HTML stand for?",
+        answers: [
+            { text: "Hand Text Makeup Language", correct: false },
+            { text: "Hyper Text Markup Language", correct: true },
+            { text: "Hard Text Markdown Language", correct: false },
+        ]
+    },
+    {
+        question: "What does CSS stand for?",
+        answers: [
+            { text: "Cascading Style Sheeps", correct: false },
+            { text: "Cascading Sheets Style", correct: false },
+            { text: "Cascading Style Sheets", correct: true },
+        ]
+    },
+    {
+        question: "What does SQL stand for?",
+        answers: [
+            { text: "Structured Query Language", correct: true },
+            { text: "Standard Query Lamp", correct: false },
+            { text: "Styled Question Language", correct: false },
+        ]
+    }
+];
+
 // Variables for game area
 
 // Start quiz function
