24f3661252f6bd6ec1a4e5f2222a353a09a6c74c
Frida010 <>
2023-07-31 17:48:31 +0000

    add comments on js file

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+// Questions and answers for game area
+
+// Variables for game area
+
+// Start quiz function
+
+// Reset answers inside the answer buttons
+
+// Checking for the right and wrong answers 
+
+// Next buttons function
+
+// Show score function
